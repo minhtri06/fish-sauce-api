00e1dbdafb3aac02a29039dbb2fcc449edc798e2
minhtri06 <pmtri.tvv@gmail.com>
2024-03-23 19:16:11 +0700

    refactor: add process event handler

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ const ENV_CONFIG = require('./configs/env.config')
 const logger = require('./configs/logger.config')
 const { connectMongoDb } = require('./db')
 
+let server = undefined
 const start = async () => {
   try {
     // connect databases
     await connectMongoDb()
     logger.info('🍃 Connect mongodb successfully')
 
-    app.listen(ENV_CONFIG.PORT, () => {
+    server = app.listen(ENV_CONFIG.PORT, () => {
       logger.info('🍂 Server is running on port ' + ENV_CONFIG.PORT)
     })
   } catch (error) {
@@ -18,3 +19,28 @@ const start = async () => {
 }
 
 start()
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed')
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+}
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error)
+  exitHandler()
+}
+
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received')
+  if (server) {
+    server.close()
+  }
+})
