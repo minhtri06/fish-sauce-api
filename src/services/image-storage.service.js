2350a439eafce6268f9acde1658c12293c160dae
minhtri06 <pmtri.tvv@gmail.com>
2024-03-23 15:59:00 +0700

    fix: not to throw err in deleteImage

--- src/services/image-storage.service.js
+++ src/services/image-storage.service.js
@@ -3,7 +3,7 @@ const { IMAGES_FULL_PATH } = require('../common/constants')
 
 const deleteImage = (imageName) => {
   fs.unlink(IMAGES_FULL_PATH + '/' + imageName, (err) => {
-    if (err) throw err
+    console.log(err)
   })
 }
 
